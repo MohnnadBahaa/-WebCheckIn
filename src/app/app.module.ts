045ed93fe383512de012875b35132defac1869e1
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-02-12 00:40:04 +0200

    (feat) add the Animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerTicketsComponent } from './passenger-tickets/passenger-tickets.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { PassengerTicketsComponent } from './passenger-tickets/passenger-tickets
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    [MatButtonModule,MatCheckboxModule,MatCardModule,MatIconModule,MatGridListModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/passenger-tickets/passenger-tickets.component.ts
+++ src/app/passenger-tickets/passenger-tickets.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, state, animate, style } from '@angular/animations';
+
 
 @Component({
   selector: 'app-passenger-tickets',
   templateUrl: './passenger-tickets.component.html',
-  styleUrls: ['./passenger-tickets.component.css']
+  styleUrls: ['./passenger-tickets.component.css'],
+  animations:[]
 })
 export class PassengerTicketsComponent implements OnInit {
 
@@ -11,5 +14,11 @@ export class PassengerTicketsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // variable section
+
+  public destination :string='';
+  public tickets=[{destination:'LAX',country:'BJS',gate1:'PEK T1',gate2:'LAX 8',depart:'9:30',arrive:'18:30',date:'19 NOV'}
+  ,{destination:'LAX',country:'BJS',gate1:'PEK T1',gate2:'LAX 8',depart:'9:30',arrive:'18:30',date:'19 NOV'}];
 
 }
