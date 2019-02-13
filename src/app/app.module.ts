028c2063aba03ffaa5ff6c21999c90e58c38fe06
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-02-13 13:31:31 +0200

    (Refactor the passenger-ticket component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerTicketsComponent } from './passenger-tickets/passenger-tickets.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MDBBootstrapModule,
+    FlexLayoutModule,
 
     [MatButtonModule,MatCheckboxModule,MatCardModule,MatIconModule,MatGridListModule]
   ],
--- src/app/passenger-tickets/passenger-tickets.component.ts
+++ src/app/passenger-tickets/passenger-tickets.component.ts
@@ -1,24 +1,175 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, state, animate, style } from '@angular/animations';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  transition,
+  state,
+  animate,
+  style
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-passenger-tickets',
-  templateUrl: './passenger-tickets.component.html',
-  styleUrls: ['./passenger-tickets.component.css'],
-  animations:[]
+  selector: "app-passenger-tickets",
+  templateUrl: "./passenger-tickets.component.html",
+  styleUrls: ["./passenger-tickets.component.css"],
+  animations: []
 })
 export class PassengerTicketsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+
   }
 
   // variable section
 
-  public destination :string='';
-  public tickets=[{destination:'LAX',country:'BJS',gate1:'PEK T1',gate2:'LAX 8',depart:'9:30',arrive:'18:30',date:'19 NOV'}
-  ,{destination:'LAX',country:'BJS',gate1:'PEK T1',gate2:'LAX 8',depart:'9:30',arrive:'18:30',date:'19 NOV'}];
+  // Test Data
+  public destination: string = "";
+  public tickets = [
+    {
+      destination: "LAX",
+      country: "BJS",
+      gate1: "PEK T1",
+      gate2: "LAX 8",
+      depart: "9:30",
+      arrive: "18:30",
+      date: "19 NOV"
+    },
+    {
+      destination: "LAX",
+      country: "BJS",
+      gate1: "PEK T1",
+      gate2: "LAX 8",
+      depart: "9:30",
+      arrive: "18:30",
+      date: "19 NOV"
+    }
+  ];
 
+  // Css Class buttons
+  public buttonClassRefund = {
+    RefundButton: true,
+    animated2: false,
+    swing: false,
+    zoomOut:false,
+    animated3:false,
+  };
+  public buttonClassChange = {
+    ChangeButton: true,
+    animated2: false,
+    swing: false,
+    zoomOut:false,
+    animated3:false,
+  };
+  public buttonClassCheckIn = {
+    CheckInButton: true,
+    animated2: false,
+    swing: false,
+    zoomOut:false,
+    animated3:false,
+  };
+  public ticketBody = {
+    card: true,
+    animated3: false,
+    tada: false,
+    hinge:false,
+    animated:false
+  };
+
+  // the index of the hover ticket
+  public selectTicketIndex = -1;
+  public selectButtonName='';
+  public selectTicketBodyIndex=-1;
+
+  // Method section
+
+  // change the class when the user hover on the button
+  inHover(i, event) {
+    let value = event.target.getAttribute("name");
+    this.selectButtonName=value;
+    this.selectTicketIndex = i;
+    if (value === "Refund") {
+      setTimeout(() => {
+        this.buttonClassRefund.animated2 = true;
+        this.buttonClassRefund.swing = true;
+      }, 500);
+    } else if (value === "Change") {
+      setTimeout(() => {
+        this.buttonClassChange.animated2 = true;
+        this.buttonClassChange.swing = true;
+      }, 500);
+    } else if (value === "CheckIn") {
+      setTimeout(() => {
+        this.buttonClassCheckIn.animated2 = true;
+        this.buttonClassCheckIn.swing = true;
+      }, 500);
+    }
+  }
+
+  // change the class when the user hover out the button
+  outHover() {
+    this.buttonClassRefund.animated2 = false;
+    this.buttonClassRefund.swing = false;
+    this.buttonClassCheckIn.animated2 = false;
+    this.buttonClassCheckIn.swing = false;
+    this.buttonClassChange.animated2 = false;
+    this.buttonClassChange.swing = false;
+    this.selectTicketIndex = -1;
+    this.selectButtonName='';
+  }
+
+// change the class when the user hover on the ticket body
+  inHoverTicketBody(i){
+    console.log(i)
+    this.selectTicketBodyIndex=i;
+    this.ticketBody.animated3=true;
+    this.ticketBody.tada=true;
+  }
+  // change the class when the user hover out the ticket body
+  outHoverTicketBody(i){
+    console.log(i)
+    this.selectTicketBodyIndex=-1;
+    this.ticketBody.animated3=false;
+    this.ticketBody.tada=false;
+  }
+
+  // when the user click the button
+  clickButton(i,event){
+   let value= event.currentTarget.name;
+   console.log(value)
+   if (value === "Refund") {
+
+      this.buttonClassRefund.animated2 = false;
+      this.buttonClassRefund.swing = false;
+      this.buttonClassRefund.zoomOut=true;
+      this.buttonClassRefund.animated3=true;
+
+  } else if (value === "Change") {
+
+      this.buttonClassChange.animated2 = false;
+      this.buttonClassChange.swing = false;
+      this.buttonClassChange.zoomOut=true;
+      this.buttonClassChange.animated3=true;
+
+  } else if (value === "CheckIn") {
+
+      this.buttonClassCheckIn.animated2 = false;
+      this.buttonClassCheckIn.swing = false;
+      this.buttonClassCheckIn.zoomOut=true;
+      this.buttonClassCheckIn.animated3=true;
+
+  }
+
+  }
+
+    // when the user click the ticket body
+    ticketBodyClick(){
+      this.ticketBody.animated3=false;
+      this.ticketBody.tada=false;
+      this.ticketBody.hinge=true;
+      this.ticketBody.animated=true;
+
+
+    }
 }
+
+
